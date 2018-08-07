a403e26fa9aa3b50420c44a7f1215a256c8a5336
Eve Essex <eve.essex@gmail.com>
2018-08-07 14:13:10 -0400

    use styled components for url input

--- src/client/components/rich_text/components/input_url.jsx
+++ src/client/components/rich_text/components/input_url.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
 import colors from '@artsy/reaction/dist/Assets/Colors'
-import { RemoveButton } from 'client/components/remove_button'
+import { RemoveButton, RemoveButtonContainer } from 'client/components/remove_button'
 
 export class TextInputUrl extends Component {
   static propTypes = {
@@ -40,17 +40,14 @@ export class TextInputUrl extends Component {
 
     return (
       <div>
-        <div
-          onClick={onClickOff}
-          className='TextInputUrl__bg'
-        />
+        <BackgroundOverlay onClick={onClickOff} />
         <TextInputUrlContainer
           className='TextInputUrl'
-          top={selectionTarget.top}
-          left={selectionTarget.left}
+          top={selectionTarget && selectionTarget.top}
+          left={selectionTarget && selectionTarget.left}
         >
-          <div className='TextInputUrl__input'>
-            <input
+          <InputContainer>
+            <Input
               autoFocus
               className='bordered-input'
               value={url}
@@ -68,14 +65,14 @@ export class TextInputUrl extends Component {
                 background={colors.grayMedium}
               />
             }
-          </div>
+          </InputContainer>
 
-          <button
+          <Button
             className='add-link'
             onMouseDown={this.confirmLink}
           >
             Apply
-          </button>
+          </Button>
         </TextInputUrlContainer>
       </div>
     )
@@ -85,4 +82,60 @@ export class TextInputUrl extends Component {
 const TextInputUrlContainer = styled.div`
   top: ${props => `${props.top}px` || 0};
   left: ${props => `${props.left}px` || 0};
+  position: absolute;
+  background-color: black;
+  height: 50px;
+  width: 400px;
+  padding: 10px;
+  display: flex;
+  z-index: 10;
+
+  ${RemoveButtonContainer} {
+    position: absolute;
+    right: 10px;
+    width: 20px;
+    height: 20px;
+    top: 5px;
+  }
+`
+
+const Input = styled.input`
+  background-color: white;
+  width: 300px;
+  max-width: 300px;
+  height: 30px;
+  font-size: 15px;
+`
+
+const InputContainer = styled.div`
+  position: relative;
+`
+
+const BackgroundOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`
+
+const Button = styled.button`
+  border: 0;
+  padding: 0;
+  width: 80px;
+  height: 30px;
+  text-align: center;
+  transition: color .15s;
+
+  &::after {
+    content: '';
+    width: 0;
+    height: 0;
+    border-left: 7px solid transparent;
+    border-right: 7px solid transparent;
+    border-bottom: 7px solid black;
+    position: absolute;
+    top: -7px;
+    left: 50%;
+  }
 `
