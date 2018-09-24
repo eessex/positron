2c8df22d8a392e236a2b84de2e428f679924c40c
Eve Essex <eve.essex@gmail.com>
2018-09-24 06:51:42 -0400

    try adding new section-container

--- src/client/apps/edit/components/content/sections/text/index2.tsx
+++ src/client/apps/edit/components/content/sections/text/index2.tsx
@@ -1,4 +1,4 @@
-import { Text } from '@artsy/reaction/dist/Components/Publishing/Sections/Text'
+// import { Text } from '@artsy/reaction/dist/Components/Publishing/Sections/Text'
 import { RichText } from 'client/components/draft/rich_text/rich_text'
 import { richTextBlockNodes } from 'client/components/draft/rich_text/utils/utils'
 import React from 'react'
@@ -18,17 +18,17 @@ export const SectionText = (props: any) => {
 
   return (
     // maybe hide tooltips?
-    <Text layout={article.layout}>
-      <RichText
-        allowedBlocks={getBlockMap(article.layout, isInternalChannel)}
-        allowedStyles={['B', 'I', 'S']}
-        hasLinks
-        hasFollowButton={isInternalChannel}
-        html={section.body || ''}
-        isDark={isDark}
-        onChange={args => args}
-      />
-    </Text>
+    // <Text layout={article.layout}>
+    <RichText
+      allowedBlocks={getBlockMap(article.layout, isInternalChannel)}
+      allowedStyles={['B', 'I', 'S']}
+      hasLinks
+      hasFollowButton={isInternalChannel}
+      html={section.body || ''}
+      isDark={isDark}
+      onChange={args => args}
+    />
+    // </Text>
   )
 }
 
